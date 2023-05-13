13134d1eecc614a3aab58b165e85fce3e30659d8
lighttttt <yxu70832@usc.edu>
2023-05-13 15:42:56 -0400

    code style fixes

--- main.cpp
+++ main.cpp
@@ -8,54 +8,59 @@
  */
 
 
-#include "main.hpp"
+#include <osquery/core/system.h>
+#include <osquery/sdk/sdk.h>
+#include <osquery/sql/dynamic_table_row.h>
 #include "helpers.cpp"
 
 using namespace osquery;
 
-TableColumns PingTable::columns() const {
-  return {
-      std::make_tuple("host", TEXT_TYPE, ColumnOptions::DEFAULT),
-      std::make_tuple("latency", TEXT_TYPE, ColumnOptions::DEFAULT),
-  };
-  }
+class PingTable : public TablePlugin {
+private:
+  TableColumns columns() const {
+    return {
+        std::make_tuple("host", TEXT_TYPE, ColumnOptions::DEFAULT),
+        std::make_tuple("latency", TEXT_TYPE, ColumnOptions::DEFAULT),
+    };
+    }
 
-TableRows PingTable::generate(QueryContext& request) {
-  TableRows results;
+  TableRows generate(QueryContext& request) {
+    TableRows results;
 
-  auto r = make_table_row();
-  if (!request.hasConstraint("host")) {
-    return results;
-  }
+    auto r = make_table_row();
+    if (!request.hasConstraint("host")) {
+      return results;
+    }
 
-  auto host = request.constraints["host"].getAll()[0].expr;
-  if (!isValidHost(host)) {
+    auto host = request.constraints["host"].getAll()[0].expr;
+    if (!isValidHost(host)) {
+      r["host"] = host;
+      r["latency"] = "INVALID HOST";
+      results.push_back(std::move(r));
+      return results;
+    }
+
+    auto ping_query = "ping -c 4 " + host + " | tail -1| awk '{print $4}' | cut -d '/' -f 2";
+
+    std::string latency{};
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    ping_query.append(" 2>&1"); 
+
+    stream = popen(ping_query.c_str(), "r");
+    if (stream) {
+        while (!feof(stream))
+            if (fgets(buffer, max_buffer, stream) != NULL) latency.append(buffer);
+        pclose(stream);
+    }
+
+    latency.erase(latency.length() - 1);
     r["host"] = host;
-    r["latency"] = "INVALID HOST";
+    r["latency"] = latency;
     results.push_back(std::move(r));
     return results;
-  }
-
-  auto ping_query = "ping -c 4 " + host + " | tail -1| awk '{print $4}' | cut -d '/' -f 2";
-
-  std::string latency{};
-  FILE * stream;
-  const int max_buffer = 256;
-  char buffer[max_buffer];
-  ping_query.append(" 2>&1"); 
-
-  stream = popen(ping_query.c_str(), "r");
-  if (stream) {
-      while (!feof(stream))
-          if (fgets(buffer, max_buffer, stream) != NULL) latency.append(buffer);
-      pclose(stream);
-  }
-
-  latency.erase(latency.length() - 1);
-  r["host"] = host;
-  r["latency"] = latency;
-  results.push_back(std::move(r));
-  return results;
+  };
 };
 
 REGISTER_EXTERNAL(PingTable, "table", "ping");
